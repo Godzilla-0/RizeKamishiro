fdab5584fd0d15b22ac413db4359dcd993b4f531
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2020-12-18 17:22:27 +0530

    Initialize beta

---
+++ README.md
@@ -0,0 +1,30 @@
+# Julia - A Telegram Bot Project
+
+> ⭐️ Thanks to everyone who has starred the project, it means a lot !
+
+##### Its a request to everyone who is using this free software and maintaining their own forks/imports from this repository to strictly comply with the LICENCE associated with this repository !<br /><br />If you are using an import your repository should contain a link to the [main repository](https://github.com/MissJuliaRobot/MissJuliaRobot) in the README.
+
+[![AGPLv3 license](https://img.shields.io/github/license/MissJuliaRobot/MissJuliaRobot)](https://www.gnu.org/licenses/agpl-3.0.en.html)
+[![CodeFactor](https://www.codefactor.io/repository/github/missjuliarobot/missjuliarobot/badge)](https://www.codefactor.io/repository/github/missjuliarobot/missjuliarobot)
+[![Hits](https://hitcounter.pythonanywhere.com/count/tag.svg?url=https%3A%2F%2Fgithub.com%2FMissJuliaRobot%2FMissJuliaRobot.git)](https://github.com/MissJuliaRobot/MissJuliaRobot)
+[![Total Size](https://github-size-badge.herokuapp.com/MissJuliaRobot/MissJuliaRobot.svg)](https://github.com/MissJuliaRobot/MissJuliaRobot)
+[![Python 3.8](https://img.shields.io/badge/python->=3.8.3-blue.svg)](https://www.python.org/downloads/release/python-383/)
+[![Maintenance](https://img.shields.io/badge/maintained-yes-yellow.svg)](https://github.com/MissJuliaRobot/MissJuliaRobot)
+
+**Blazingly fast Telegram Group Management Bot based on [Telethon](https://github.com/LonamiWebs/Telethon) coupled with [Sqlalchemy](https://github.com/sqlalchemy/sqlalchemy) and [MongoDB](https://github.com/mongodb/mongo)**
+
+Can be found on telegram as [Julia](https://t.me/MissJuliaRobot)
+
+Deploy Julia to Heroku:
+
+<p align="left"><a href="https://heroku.com/deploy?template=https://github.com/MissJuliaRobot/MissJuliaRobot/tree/master"> <img src="https://www.herokucdn.com/deploy/button.svg" alt="Deploy to Heroku" /></a></p>
+
+## Credits
+**Thanks to these projects which helped me a lot to make this awesome bot**
+
+- [Uniborg](https://github.com/SpEcHiDe/UniBorg)<br />
+- [Telegram Paperplane](https://github.com/RaphielGang/Telegram-Paperplane)<br />
+- [Paperplane Extended](https://github.com/AvinashReddy3108/PaperplaneExtended)<br />
+- [TelethonianBotExt(Silvia)](https://github.com/Lonami/TelethonianBotExt)
+
+**And Last Of All thanks to [@lonami](http://t.me/lonami) for creating [Telethon](https://github.com/LonamiWebs/Telethon) !**
---
+++ julia/__init__.py
@@ -0,0 +1,95 @@
+import logging
+import os
+import sys
+import time
+from logging import basicConfig
+from logging import DEBUG
+from logging import getLogger
+from logging import INFO
+
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+
+StartTime = time.time()
+CMD_LIST = {}
+CMD_HELP = {}
+LOAD_PLUG = {}
+BOT_VERSION = "1.0.2"
+
+# enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+LOGGER = logging.getLogger(__name__)
+ENV = bool(os.environ.get("ENV", True))
+
+if ENV:
+    TOKEN = os.environ.get("TOKEN", None)
+    try:
+        OWNER_ID = int(os.environ.get("OWNER_ID", None))
+    except ValueError:
+        raise Exception("Your OWNER_ID env variable is not a valid integer.")
+
+    GBAN_LOGS = os.environ.get("GBAN_LOGS", None)
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
+
+    try:
+        SUDO_USERS = {int(x) for x in os.environ.get("SUDO_USERS", "").split()}
+    except ValueError:
+        raise Exception(
+            "Your sudo users list does not contain valid integers.")
+
+    API_KEY = os.environ.get("API_KEY", None)
+    API_HASH = os.environ.get("API_HASH", None)
+
+    OPENWEATHERMAP_ID = os.environ.get("OPENWEATHERMAP_ID", None)
+    DB_URI = os.environ.get("DATABASE_URL")
+    YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
+    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
+    OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
+    WOLFRAM_ID = os.environ.get("WOLFRAM_ID", None)
+
+    LYDIA_API_KEY = os.environ.get("LYDIA_API_KEY", None)
+    tbot = TelegramClient("julia", API_KEY, API_HASH)
+
+    SUDO_USERS = list(SUDO_USERS)
+    REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
+    IBM_WATSON_CRED_URL = os.environ.get("IBM_WATSON_CRED_URL", None)
+    IBM_WATSON_CRED_PASSWORD = os.environ.get("IBM_WATSON_CRED_PASSWORD", None)
+    WALL_API = os.environ.get("WALL_API", None)
+    CHROME_DRIVER = os.environ.get("CHROME_DRIVER", None)
+    GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", None)
+    CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
+    TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
+
+    STRING_SESSION = os.environ.get("STRING_SESSION", None)
+    MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
+    TEMPORARY_DATA = os.environ.get("TEMPORARY_DATA", None)
+
+    CONSOLE_LOGGER_VERBOSE = os.environ.get("CONSOLE_LOGGER_VERBOSE", "False")
+
+    if CONSOLE_LOGGER_VERBOSE:
+        basicConfig(
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=DEBUG
+        )
+    else:
+        basicConfig(
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=INFO
+        )
+
+    LOGS = getLogger(__name__)
+
+    if STRING_SESSION:
+        ubot = TelegramClient(StringSession(STRING_SESSION), API_KEY, API_HASH)
+    else:
+        sys.exit(1)
+
+    try:
+        ubot.start()
+    except BaseException:
+        print("Network Error !")
+        sys.exit(1)
+
+else:
+    sys.exit(1)
